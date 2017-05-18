5a8e1b91158b83161f01ec401e0147b1281b4dab
神楽坂雅詩 <cyscxchope@gmail.com>
2017-05-18 19:26:48 +0800

    完成页面显示

--- page.php
+++ page.php
@@ -1,16 +1,24 @@
 <?php get_header(); ?>
-<!-- Column 1 / Content -->
-<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
-<h2><?php the_title(); ?></h2>
-<div>
-    <?php the_content(); ?>
-    <!-- Contact Form -->
-    <div id="commentsbox"><p><b>评论区</b>（如果看不到，请先想办法访问谷歌233）</p><?php comments_template(); ?></div>
-</div>
-<?php else : ?>
-<div>
-    没有找到你想要的页面！
-</div>
-<?php endif; ?>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+
+	<div id="scrollpic" class="singleleftbox">
+		<!-- Column 1 /Content -->
+		<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+		<div id="singlebdiv">
+			<div id="singlerightdiv">
+				<span id="srdtop">
+					<div id="srdtitle"><?php the_title(); ?></div>
+					<div id="srddate"><?php the_time('Y年n月j日') ?> &bull; <?php $category = get_the_category(); echo '<a href="'.get_category_link($category[0]->term_id ).'">'.$category[0]->cat_name.'</a>'; ?><?php edit_post_link('编辑', ' &bull; ', ''); ?></div>
+				</span>
+				<div id="srdcontentbox"><div id="srdcontent"><?php the_content(); ?></div></div>
+			</div>
+		</div>
+		<script type="text/javascript" src="<?php bloginfo('template_url'); ?>/single.js"></script>
+		<div id="commentsbox"><p><b>评论区</b>（如果看不到，请先想办法访问谷歌233）</p><?php comments_template(); ?></div>
+	    <?php else : ?>
+	    <div class="errorbox">
+	        没有文章！
+	    </div>
+	    <?php endif; ?>
+	</div>
+    <div id="scrolltextsingle"><?php get_sidebar(); ?></div>
+	<?php get_footer(); ?>
