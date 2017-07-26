6b99d3b1906ed56997e0e36316e6e752342fc404
神楽坂雅詩 <cyscxchope@gmail.com>
2017-07-26 20:56:58 +0800

    修复使用引用其他网页功能时的执行错误

--- t_includehtml.php
+++ t_includehtml.php
@@ -5,18 +5,22 @@ Template Name: 导入HTML文章
 ?>
 <?php get_header(); ?>
 <h2><?php the_title(); ?></h2>
-<div id="includehtml">
+<?php if (have_posts()) : the_post(); update_post_caches($posts); ?>
+<div class="includehtml"></div>
 <?php
 $imphtmlarr = explode("[IMP]",get_the_content());
-$imphtml = $imphtmlarr[1];
-$impdivarr = explode(",",$imphtml);
-$impdiv = "";
-if (count($impdivarr)>1) {
-    $imphtml = $impdivarr[0];
-    $impdiv = " ".$impdivarr[1];
+if (count($imphtmlarr) > 1) {
+    $imphtml = $imphtmlarr[1];
+    $impdivarr = explode(",",$imphtml);
+    $impdiv = "";
+    if (count($impdivarr)>1) {
+        $imphtml = $impdivarr[0];
+        $impdiv = " ".$impdivarr[1];
+    }
+    echo '<script type="text/javascript">$(".includehtml").load("'.$imphtml.'","'.$impdiv.'")</script>';
+} else {
+    echo "错误：没有写 [IMP] 关键字";
 }
-echo '<script type="text/javascript">$("#includehtml").load("'.$imphtml.$impdiv.'")</script>';
-?>
-</div>
+endif; ?>
 <?php get_footer(); ?>
 <script>
