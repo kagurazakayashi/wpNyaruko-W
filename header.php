b019c494320ea5e29f54cfe037f37f616bb82a33
神楽坂雅詩 <cyscxchope@gmail.com>
2018-08-14 14:06:41 +0800

    在网页标题中始终显示网站名称

--- header.php
+++ header.php
@@ -11,21 +11,19 @@ if(@$wpNyarukoOption['wpNyarukoPHPDebug']!='') {
 <?php include_once("KagurazakaYashi.php"); ?>
 <meta name="viewport" content="width=device-width,initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 <?php echo @$wpNyarukoOption['wpNyarukoHeader']; ?>
-<title>
-<?php if ( is_home() ) {
-        bloginfo('name'); echo " - "; bloginfo('description');
-    } elseif ( is_category() ) {
-        single_cat_title(); echo " - "; bloginfo('name');
-    } elseif (is_single() || is_page() ) {
-        single_post_title();
-    } elseif (is_search() ) {
-        echo "搜索结果"; echo " - "; bloginfo('name');
-    } elseif (is_404() ) {
-        echo '页面未找到!';
-    } else {
-        wp_title('',true);
-    } ?>
-</title>
+<title><?php if ( is_home() ) {
+    bloginfo('name'); echo " - "; bloginfo('description');
+} elseif ( is_category() ) {
+    single_cat_title(); echo " - "; bloginfo('name');
+} elseif (is_single() || is_page() ) {
+    single_post_title(); echo " - "; bloginfo('name');
+} elseif (is_search() ) {
+    echo "搜索结果"; echo " - "; bloginfo('name');
+} elseif (is_404() ) {
+    echo '页面未找到!';
+} else {
+    wp_title('',true);
+} ?></title>
 <!-- Stylesheets -->
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
 <?php include_once("style.php");
