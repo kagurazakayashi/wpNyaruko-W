45cf7b87b9dba8fbab44825409014fc11beb2d35
神楽坂雅詩 <cyscxchope@gmail.com>
2018-12-19 22:05:43 +0800

    兼容 EnlighterJS Syntax Highlighter

--- single.js
+++ single.js
@@ -22,21 +22,22 @@ function singleload() {
 	});
 }
 // singleload();
-
+function textboxsethtml(texts,newhtml) {
+    if (newhtml != "") texts.html(newhtml);
+}
 function contentformat() {
     var texts = $("#pagetext");
     var alltext = texts.html();
     var textlines = alltext.split('\n');
     var firstline = true;
-    var spacespan = '<span class="pagetextindent"></span>';
+    var spacespan = '<span class="racing_indent"></span>';
     var noformats = ["[noformat]","[noformat=all]","<script","<link","[noformat=img]","[noformat=indent]","[nyarukotabloid]"];
     var isnoformat = false;
     var newhtml = "";
-    // console.log("textlines[0]",textlines[0]);
+    var codemode = false;
     for (let noformatj = 2; noformatj <= 3; noformatj++) {
         const noformat = noformats[noformatj];
         if (textlines[0].length >= noformat.length && textlines[0].substr(0,noformat.length) == noformat) {
-            console.log(noformat);
             isnoformat = true;
             break;
         }
@@ -45,7 +46,6 @@ function contentformat() {
         for (let noformati = 0; noformati < noformats.length; noformati++) {
             const noformat = noformats[noformati];
             if (textlines[0].length >= noformat.length && textlines[0].substr(0,noformat.length) == noformat) {
-                console.log(noformat);
                 isnoformat = true;
                 newhtml = alltext.substr(noformat.length,alltext.length-noformat.length);
                 break;
@@ -60,11 +60,33 @@ function contentformat() {
         console.log(noformats[5]);
         spacespan = "";
     }
+    var debug_noformat = window.location.hash == "#noformat" ? true : false;
+    var debug_format = window.location.hash == "#format" ? true : false;
+    if (debug_noformat) {
+        console.log("[DEBUG MODE] noformat");
+        isnoformat = true;
+    }
+    if (debug_format) {
+        console.log("[DEBUG MODE] format");
+        isnoformat = false;
+    }
     if (!isnoformat) {
+        console.log("format...");
         for (let line = 0; line < textlines.length; line++) {
             var nowline = textlines[line];
             var nowlinetype = nowline.replace(/(^\s*)|(\s*$)/g, "").substr(0,2);
-            //console.log(line,nowlinetype,nowline);
+            if (window.location.hash == "#formatlog") {
+                console.log(line,nowlinetype,nowline);
+            }
+            if (nowline.search('<pre class="EnlighterJS') != -1) {
+                codemode = true;
+            } else if (nowline.search('</pre>') != -1) {
+                codemode = false;
+            }
+            if (codemode) {
+                textlines[line] = nowline;
+                continue;
+            }
             if (nowlinetype == "<i" || nowlinetype == "<s" || nowlinetype == "<d" || nowlinetype == "<v" || nowlinetype == "") {
                 if (firstline && nowlinetype != "") {
                     if (firstline) {
@@ -95,18 +117,36 @@ function contentformat() {
                 }
             } else if (nowlinetype != "" && nowlinetype.substr(0,1) != "<") {
                 textlines[line] = spacespan + nowline;
+            } else {
+                textlines[line] = nowline;
             }
         }
-        newhtml = textlines.join('\n');
-        newhtml = newhtml.replace(/\n\n/g, '<br/>');
-        texts.html(newhtml);
+        // newhtml = textlines.join('<br/>');
+        newhtml = newhtml.replace(/<br\/><br\/>/g, '<br/>');
+        newhtml = newhtml.replace(/<br\/><p/g, '<p');
+        newhtml = newhtml.replace(/<br\/> 	<li>/g, '<li>');
+        newhtml = newhtml.replace(/<br\/><\/ul><br\/>/g, '</ul>');
+        newhtml = newhtml.replace(/<br\/><\/ol><br\/>/g, '</ol>');
+        newhtml = newhtml.replace(/<\/h1><br\/>/g, '</h1>');
+        newhtml = newhtml.replace(/<\/h2><br\/>/g, '</h2>');
+        newhtml = newhtml.replace(/<\/h3><br\/>/g, '</h3>');
+        newhtml = newhtml.replace(/<\/h4><br\/>/g, '</h4>');
+        newhtml = newhtml.replace(/<\/h5><br\/>/g, '</h5>');
+        newhtml = newhtml.replace(/<\/h6><br\/>/g, '</h6>');
+        textboxsethtml(texts,newhtml);
         if (!(textlines[0].length >= noformats[2].length && textlines[0].substr(0,noformats[2].length) == noformats[2])) {
             $("#pagetext img").each(function(){
                 $(this).addClass("pagetext_autosizeimg");
             });
         }
+        var scrolltable = $(".scrolltable");
+        for (let sti = 0; sti < scrolltable.length; sti++) {
+            const scrolltablen = $(scrolltable[sti]);
+            const scrolltablet = $(".scrolltable table")[sti];
+            scrolltablen.html(scrolltablet);
+        }
     } else {
-        texts.html(newhtml);
+        textboxsethtml(texts,newhtml);
     }
 }
 contentformat();
